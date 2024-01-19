e0563f905f70830b30ff5e1488ee287c08dd1fbb
itzel <itzelalwarafii@gmail.com>
2024-01-19 11:01:19 -0800

    switcher

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
 // Write your DOM code here!
+
+const btn1 = document.querySelector('#btn1')
+btn1.addEventListener('click',() => {
+    document.body.className = ('red')
+ 
+})
+const btn2 = document.querySelector('#btn2')
+btn2.addEventListener('click',() => {
+    document.body.className =('white')
+})
+const btn3 = document.querySelector('#btn3')
+btn3.addEventListener('click',() => {
+    document.body.className= ('blue')
+   
+})
+const btn4 = document.querySelector('#btn4')
+btn4.addEventListener('click',() => {
+    document.body.className = ('yellow')
+
+})
+const btn5 = document.querySelector('#btn5')
+btn5.addEventListener('click',() => {
+    document.body.className = ('black')
+
+})
+const btn6 = document.querySelector('#btn6')
+btn6.addEventListener('click',() => {
+    document.body.className = ('pink')
+
+})
+
+const btn7 = document.querySelector('#btn7')
+btn7.addEventListener('click',() => {
+    document.body.className = ('pinkGrd')
+
+})
+
